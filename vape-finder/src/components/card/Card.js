8f8d23c22ef6a070e104ef26dae78d04a0dd3b83
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-01 10:18:28 -0800

    added product details, styling to card

--- vape-finder/src/components/card/Card.js
+++ vape-finder/src/components/card/Card.js
@@ -1,7 +1,33 @@
 import './card.css'
 
+//can also link images using import or require() 
+//import demoImage from './demo.webp'; 
+
 function Card( {product} ) {
-  return (<div className="card">{product}</div>);
+  const {id, name, brand, category, img, price, last_updated, source} = product
+ 
+  return (
+    <div className="card">
+
+      <span>{source}<br/></span>
+      <span>{brand}<br/></span>
+      <span>{category}<br/></span>
+      
+      <span className="title">{name}<br/></span>   
+      <span>${price}<br/></span>
+
+      <img class="product_img"
+        src={img}
+        alt="Grapefruit slice atop a pile of other slices">
+      </img>
+
+      <div className="card_footer">
+        <span>last updated: {last_updated}</span>
+      </div>
+      
+    </div>
+  );
 }
 
 export default Card;
+
