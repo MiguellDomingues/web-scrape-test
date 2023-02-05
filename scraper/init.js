8490838ba635c45fac440fda79096eb727395d20
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-04 19:39:42 -0800

    cleanup unused comments, packages

--- scraper/init.js
+++ scraper/init.js
@@ -1,46 +1,14 @@
 
 /*
-(async = () => {
-
-    let tbv = false, sv = false, ezv = false
-
-    function done(){      
-        if(sv && tbv && ezv){
-          require("./inventory").addInventoriesToDB()
-        }
-      }
-
-      require("./scripts/ezvape").then( ()=>{
-        ezv = true
-        done()
-      })
-    
-      require("./scripts/thunderbirdvapes").then( ()=>{
-        tbv = true
-        done()
-      })
-    
-      require("./scripts/surreyvapes").then( ()=>{
-        sv = true
-        done()
-      })
-})()
-*/
-
-//require("./scripts/inventory")
-
-/*
 execute scraping scripts and write inventory json to db upon completion
 - ezvape takes about 5 minutes to fully scrape
 */
 
-
 Promise.all([
-        require("./scripts/ezvape"), 
-        require("./scripts/thunderbirdvapes"), 
-        require("./scripts/surreyvapes")
+      //require("./scripts/ezvape"), 
+      //require("./scripts/thunderbirdvapes"), 
+      //require("./scripts/surreyvapes")
     ]).then( () => {
-        require("./scripts/inventory")
-    }
-)
+      require("./scripts/inventory")
+    })
 
