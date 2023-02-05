036718f63b5b75709e80634fe0a6080a239a4314
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-04 19:28:29 -0800

    added queryql schema, root

---
+++ vape-finder-server/main.js
@@ -0,0 +1,135 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var fetchProducts = require('../database/read/readProducts.js')
+
+var app = express();
+var cors = require('cors');
+app.use( cors() );          // allow react app communicate with server on same machine/diff port
+
+// Construct a schema, using GraphQL schema language
+
+var schema = buildSchema(`
+
+  type Product {
+    id:           ID!
+    source_id:    String!                
+    source_url:   String!
+    last_updated: String! 
+    info:         ProductInfo!
+  }
+
+  type ProductInfo {
+    name:         String!                 
+    img_src:      String!               
+    price:        Float              
+    brand:        String             
+    category:     String   
+  }
+
+  type Query {
+    getProducts: [Product!]
+  }
+`);
+
+     
+
+// If Message had any complex fields, we'd put them on this object.
+class Product {
+  constructor({_id, source_id, source_url, last_updated, product_info} ) {
+
+        this.id = _id         
+        this.source_id = source_id                   
+        this.source_url = source_url   
+        this.last_updated = last_updated 
+        this.info = new ProductInfo(product_info) 
+  }
+}
+
+class ProductInfo {
+    constructor({name, img_src, price, brand, category}) {
+        this.name = name
+        this.img_src = img_src
+        this.price = price
+        this.brand = brand
+        this.category = category
+  }
+}
+
+var root = {
+    getProducts: async () => (await fetchProducts()).map( product => new Product(product)),
+};
+
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, () => {
+  console.log('Running a GraphQL API server at localhost:4000/graphql');
+});
+
+
+
+
+/*
+
+
+var query = `query getProducts {
+    id, source_id, 
+    info{
+      brand, category
+    }
+  }                       
+}`;
+
+fetch('http://localhost:4000/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({
+    query,
+    }
+  })
+})
+  .then(r => r.json())
+  .then(data => console.log('data returned:', data));
+
+
+
+
+
+
+
+
+
+
+  var query = `query GetProducts {
+    getProducts{
+        id, source_id, 
+    info{
+      brand, category
+        }   
+    }   
+  }                       
+`;
+
+fetch('http://localhost:4000/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({
+    query,
+    
+  })
+})
+  .then(r => r.json())
+  .then(data => console.log('data returned:', data));
+
+*/
