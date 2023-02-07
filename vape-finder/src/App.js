77a49ad85e221854041d7de1bece4afb1a165ab4
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-06 19:43:12 -0800

    moved graphql query to app root

--- vape-finder/src/App.js
+++ vape-finder/src/App.js
@@ -3,14 +3,30 @@ import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import Body from './components/body/Body'
 
+import { useQuery } from '@apollo/client';
+import { GET_PRODUCTS } from './queries/getProducts.js'
+
 import './app.css'
 
 function App() {
+
+  const tags = []
+
+  const query = useQuery(GET_PRODUCTS, { variables: { tags } });
+
+  const onTagSelected = (tag) => { 
+    console.log(tag)
+    tags.push(tag)
+    
+    query.refetch({ tags: tags  })
+
+  }
+  
   return (
     <div className="page"> 
       <div className="app">
-        <Header/>
-        <Body/>
+        <Header selected={onTagSelected}/>
+        <Body query={query}/>
         <Footer/>
       </div>
     </div>
