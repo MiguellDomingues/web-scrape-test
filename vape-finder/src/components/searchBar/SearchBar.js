4a14fb0c8affe9c34fcbdea45c3a70e290cc9488
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-06 19:40:51 -0800

    added dropdown css for category/brands/shops

--- vape-finder/src/components/searchBar/SearchBar.js
+++ vape-finder/src/components/searchBar/SearchBar.js
@@ -1,30 +1,64 @@
 import './search_bar.css'
 
+
 const categories = [
     'Disposables','E-Juice','Hardware','Pods','Vape Kits','Accessories'
 ]
 
-function SearchBar(){
+const brands = [
+    'ALLO', 'GORE', 'JUUL', 'VOOPOO'
+]
 
-return (
-    <>
+const shops = [
+    'EZVAPE','SURREY VAPES', 'THUNDERBIRD VAPES'
+]
+
+
+function SearchBar( {handleTagSelected} ){
+
+   //const { categorySelected , shopSelected, brandSelected} = selected
+
+//const categorySelected = (category) => { console.log(category) }
+//const brandSelected = (brand) => { console.log(brand) }
+//const shopSelected = (shop) => { console.log(shop) }
+
+return (<>
 
         <div className="search_bar_categories">
             {categories.map( (category, idx) =>
-                <span className="search_bar_category" key={idx}>{category}  </span>
-            )}
-
+                <span className="search_bar_category cursor_hand" 
+                      key={idx} 
+                      onClick={ () => handleTagSelected(category) }>
+                    {category}  
+                </span>)}
         </div>
 
         <div className="search_bar_shops_about">
-           <span className="shops">Shops</span> 
+            <div className="brands cursor_hand">
+                Brands
+                <div className="dropdown-arrow"></div>
+                <div className="dropdown-content">    
+                    {brands.map( (brand, idx)=>
+                    <span key={idx} onClick={()=>handleTagSelected(brand)}>
+                        {brand}
+                    </span>)}                  
+                </div>
+            </div>
+            <div className="shops cursor_hand">
+                Shops
+                <div className="dropdown-arrow"></div>
+                <div className="dropdown-content">    
+                    {shops.map( (shop, idx)=>
+                    <span key={idx} onClick={()=>handleTagSelected(shop)}>
+                        {shop}
+                    </span>)}                  
+                </div>
+            </div>        
            
-           <span className="about">About</span> 
+           <span className="about cursor_hand">About</span> 
         </div>
-
-        
-
     </>)
+
 }
 
 export default SearchBar
