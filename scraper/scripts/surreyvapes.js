b0850bd1937d5b1fa85527e4cf3e4ba72fb8c615
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-07 18:45:14 -0800

    added cleaning for scraped surryvapes products

--- scraper/scripts/surreyvapes.js
+++ scraper/scripts/surreyvapes.js
@@ -4,12 +4,9 @@ const utils = require("../utils.js")
 const DOMAIN                    = 'https://www.surreyvapes.com'
 const DATA_DIR                  = `${utils.ROOT_DATA_DIR}/surreyvapes`
 
- //create the root data dir if it does not exist
- //if (!fs.existsSync(DATA_DIR)) fs.mkdirSync(DATA_DIR, { recursive: true });  
+utils.createDirs([DATA_DIR])
 
- utils.createDirs([DATA_DIR])
-
-const ALL_PRODUCTS_FILE_NAME    = 'products'
+const ALL_PRODUCTS_FILE_NAME    = 'raw_products'
 const INVENTORY_FILE_NAME       = 'surreyvapes'
 const LOG_FILE_NAME             = 'surreyvapes'
 
@@ -41,6 +38,73 @@ function scrapeProductInfo(html) {
   return products
 }
 
+function clean(raw_products){
+
+    // remove non-vape products, such as rolling-papers, vaporizers, lighters, butane, etc
+    // this is acquired from analysis of categories
+    let includes = [
+        'Accessories,510ThreadBatteries',
+        'Accessories,Batteries&Chargers',
+        'E-Cigs',
+        'E-Juice',]
+
+    return raw_products
+    .filter(                    // remove no-category products
+        (p) => p.category) 
+    .map( (p)=>{                // break up category string by "/", remove first element if it's "PRODUCTS", join with ,'s
+        let arr = p.category.split("/")
+        if(arr[0] === 'PRODUCTS') arr.shift()
+        let str = arr.join(",")
+        p.category = str
+        return p })
+    .filter(                        // only include products that matches at least one of the strings in the includes arr                                                       
+        (p)=> includes.filter( (tag) => p.category.includes(tag) ).length > 0 )
+    .map(                           // clean up the brand string
+        (p) => {
+            p.brand = (p.brand === '' || p.brand.includes("The Best Vape Store In Surrey") ) ? p.name.split(" ")[0] : p.brand
+            return p})
+}
+
+module.exports = ( () => {
+    return new Promise( async (resolve) => {
+
+        const time_start = Date.now()
+
+        try{
+
+            logger.info("**************************executing " +DOMAIN+ " process***********************************************")
+            const start_page = 1
+            const end_page = 4
+    
+            const subpath = 'products'
+            const page_param = (page)=>`page=${page}`
+            const limit_param = 'limit=250'
+    
+            const urls = []
+    
+            //generate urls
+            for(let page = start_page; page <= end_page; page++)
+                urls.push(`${DOMAIN}/${subpath}?${limit_param}&${page_param(page)}`)
+            
+            //visit urls, process each url with scraper function, return array of products
+            const raw_products = (await utils.scrapePages(urls, scrapeProductInfo,logger)).flat()
+            
+            utils.writeJSON(DATA_DIR, ALL_PRODUCTS_FILE_NAME, raw_products, logger)
+            utils.writeJSON(utils.INVENTORIES_DIR, INVENTORY_FILE_NAME, clean(raw_products), logger)
+        
+        }catch(err){
+            logger.error(err)
+        }finally{
+            const time_finish = Date.now()
+            logger.info("processed " +DOMAIN+ " execution in " + (time_finish - time_start)/1000 + " seconds")
+            resolve()
+        }
+    })
+})()
+
+
+
+/*
 module.exports = (async = () => {
     return new Promise( (resolve) => {
 
@@ -79,47 +143,6 @@ module.exports = (async = () => {
         }) 
     })
 })()
-
-
-/*
-async function execute(){
-    return new Promise( (resolve) => {
-
-        logger.info("**************************executing " +DOMAIN+ " process***********************************************")
-
-        const time_start = Date.now()
-
-        const start_page = 1
-        const end_page = 4
-
-        const subpath = 'products'
-        const page_param = (page)=>`page=${page}`
-        const limit_param = 'limit=250'
-
-        const urls = []
-
-        //generate urls
-        for(let page = start_page; page <= end_page; page++)
-            urls.push(`${DOMAIN}/${subpath}?${limit_param}&${page_param(page)}`)
-        
-        //visit urls, process each url with scraper function, return array of products
-        utils.scrapePages(urls, scrapeProductInfo,logger)
-        .then( 
-            (products) => {
-                products = products.flat()
-                utils.writeJSON(DATA_DIR, ALL_PRODUCTS_FILE_NAME, products, logger)
-                utils.writeJSON(utils.INVENTORIES_DIR, INVENTORY_FILE_NAME, products, logger)
-        })
-        .catch( err => {
-            logger.error(err)
-        })
-        .finally( ()=>{
-            const time_finish = Date.now()
-            logger.info("processed " +DOMAIN+ " execution in " + (time_finish - time_start)/1000 + " seconds")
-            resolve()
-        }) 
-    })
-}
-
-module.exports = { execute }
 */
+
+
