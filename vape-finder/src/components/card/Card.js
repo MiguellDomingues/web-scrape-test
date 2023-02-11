bcd8c1d756f30ffefbb84ab3e31380b60c83d478
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-10 18:29:31 -0800

    added output formating for some card fields

--- vape-finder/src/components/card/Card.js
+++ vape-finder/src/components/card/Card.js
@@ -3,25 +3,31 @@ import './card.css'
 //can also link images using import or require() 
 //import demoImage from './demo.webp'; 
 
-const img_src = '../../../demo.webp';
+//const img_src = '../../../demo.webp';
 
 function Card( {product} ) {
   //const {id, name, brand, category, img, price, last_updated, source} = product
 
   const {id, last_updated, source_id, source_url, info } = product
-  const{ brand, category, name, price } = info
+  const{ brand, category, name, price, info_url, img_src } = info
 
+  const vendor = source_url.split("//")[1].split(".")[0]
+  const format_price = price.toFixed(2);
 
+  let format_name = name.toLowerCase().replace( brand ? brand.toLowerCase() : '', '').trim()
+  format_name = format_name.replace(/^[^A-Z0-9]+|[^A-Z0-9]+$/ig, '') //remove all start/end nonalphanumeric chars
+
+  //if(format_name.includes(' - ')) format_name = format_name.replace(' - ', ' ').trim()
+
+  //rel="noopener noreferrer" is to prevent 'tabnabbing', a kind of phishing attack
  
   return (
     <div className="card">
-
-      <span>{source_url}<br/></span>
-      <span>{brand}<br/></span>
-      <span>{category}<br/></span>
       
-      <span className="title">{name}<br/></span>   
-      <span>${price}<br/></span>
+      <a href={source_url} target="_blank" rel="noopener noreferrer">{vendor}<br/></a>
+      <span>{brand}<br/></span>     
+      <span className="title">{format_name}<br/></span>   
+      <span>${format_price}<br/></span>
 
       <img className="product_img"
         src={img_src}
