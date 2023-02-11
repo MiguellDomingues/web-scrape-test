9b8b197a75a35a6b143c059b53f72ac7e22e133b
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-10 18:26:50 -0800

    updated apollo gql schema

--- vape-finder/src/queries/getProducts.js
+++ vape-finder/src/queries/getProducts.js
@@ -5,7 +5,7 @@ export const GET_PRODUCTS = gql`
         getProducts(tags: $tags){
             id, source_id,  source_url, last_updated
         info{
-        brand, category, name, img_src, price
+            brand, category_str, name, img_src, price, info_url
             }   
         }   
     }
