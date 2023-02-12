19f97085271e84091eeb2b6f9673549655dcbb12
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-12 06:43:31 -0800

    cleaned up tbvapes, init script

--- scraper/init.js
+++ scraper/init.js
@@ -4,11 +4,76 @@ execute scraping scripts and write inventory json to db upon completion
 - ezvape takes about 5 minutes to fully scrape
 */
 
+
+const tbvapes_config = {
+   domain:              'https://www.thunderbirdvapes.com',
+   data_dir:            'thunderbirdvapes',
+   raw_products_file:   'products',
+   inventory_file:      'thunderbirdvapes',
+   log_file:            'thunderbirdvapes',
+   buckets:[
+      {
+          name: 'Juices',
+          synonyms: ['E-Liquid'] 
+      },
+      {
+          name: 'Coils',
+          synonyms: ['coil','rda','atomizer','RPM 40 Pod','Ego 1 Coil 1.0 Ohm 5/Pk','Metal RDA Stand','Crown 5 Coil']
+      },
+      {
+          name: 'Pods',
+          synonyms: ['pod',]
+      },
+      {
+          name: 'Tanks',
+          synonyms: ['tank','clearomizer']
+      },
+      {
+          name: 'Starter Kits',
+          synonyms: ['starter', 'kit','disposable','disposables']
+      },
+      {
+          name: 'Mods',
+          synonyms: ['boxes', 'boxmod', 'box mod', 'mod', 'box', 'Aegis Legend 2 200W Mod']
+      },
+      {
+          name: 'Batteries',
+          synonyms: ['battery', 'batteries','18650','18650','Evod 650mAh Battery']
+      },
+      {
+          name: 'Chargers',
+          synonyms: ['charger','charging','lush q4 charger','evod usb charger','Intellicharger I4 V2 Li-Ion/Nimh','Battery Charger','Wall Adapter','Power Bank']
+      },
+      {
+          name: 'Replacement Glass',
+          synonyms: ['glass','replacement','pyrex','replacement glass','dotAIO V2 Replacement Tank']
+      },
+      {
+          name: 'Accessories/Miscellaneous',
+          synonyms: ['wire','drip tip','cotton','apparel','mod accessories','pens','wick','adapter',
+          'screwdriver','tweezer','decorative ring','magnet connector','vaper twizer','diy tool kit','Clapton Coil Building Kit','Zipper Storage Bag','Mouthpiece Glass']
+      }
+    ],
+    utils:                      require("./utils.js"),
+    execute_scrape:             false,
+    execute_inventory:          true
+}
+
+
+
 Promise.all([
       //require("./scripts/ezvape"), 
-      //require("./scripts/thunderbirdvapes"), 
-      //require("./scripts/surreyvapes")
+      require("./scripts/thunderbirdvapes")(tbvapes_config)
+     // require("./scripts/surreyvapes")
     ]).then( () => {
-      require("./scripts/inventory")
+     // require("./scripts/inventory")
     })
+
+
+
+
+    
+
+     
+
 
--- scraper/scripts/thunderbirdvapes.js
+++ scraper/scripts/thunderbirdvapes.js
@@ -1,64 +1,41 @@
-const utils = require("../utils.js")
+module.exports = (config) => {  
 
-const DOMAIN                    = 'https://www.thunderbirdvapes.com'
-const DATA_DIR                  = `${utils.ROOT_DATA_DIR}/thunderbirdvapes`
+    const {
+        domain, 
+        data_dir,
+        raw_products_file, 
+        inventory_file, 
+        log_file, 
+        buckets, 
+        utils, 
+        execute_scrape, 
+        execute_inventory} = config
 
-//create the root data dir if it does not exist
-utils.createDirs([DATA_DIR])
+    
+    const log = utils.getLogger(log_file)
+    const _data_dir = `${utils.ROOT_DATA_DIR}/${data_dir}`
+    utils.createDirs([data_dir])
 
-const RAW_PRODUCTS_FILE_NAME    = 'products'
-const INVENTORY_FILE_NAME       = 'thunderbirdvapes'
-const LOG_FILE_NAME             = 'thunderbirdvapes'
+    return new Promise( async (resolve) => {
+        log.info("**************************executing " + domain + " process***********************************************")
 
-const logger = utils.getLogger(LOG_FILE_NAME)
+        const time_start = Date.now()
 
-const buckets = [
-    {
-        name: 'Juices',
-        synonyms: ['e-juice', //surreyvapes
-                   'ejuice',  //ezvape
-                   'e-liquid'] //tbvapes
-    },
-    {
-        name: 'Coils',
-        synonyms: ['coil','rda','atomizer','RPM 40 Pod','Ego 1 Coil 1.0 Ohm 5/Pk','Metal RDA Stand','Crown 5 Coil']
-    },
-    {
-        name: 'Pods',
-        synonyms: ['pod',]
-    },
-    {
-        name: 'Tanks',
-        synonyms: ['tank','clearomizer']
-    },
-    {
-        name: 'Starter Kits',
-        synonyms: ['starter', 'kit','disposable','disposables']
-    },
-    {
-        name: 'Mods',
-        synonyms: ['boxes', 'boxmod', 'box mod', 'mod', 'box', 'Aegis Legend 2 200W Mod']
-    },
-    {
-        name: 'Batteries',
-        synonyms: ['battery', 'batteries','18650','18650','Evod 650mAh Battery']
-    },
-    {
-        name: 'Chargers',
-        synonyms: ['charger','charging','lush q4 charger','evod usb charger','Intellicharger I4 V2 Li-Ion/Nimh','Battery Charger','Wall Adapter','Power Bank']
-    },
-    {
-        name: 'Replacement Glass',
-        synonyms: ['glass','replacement','pyrex','replacement glass','dotAIO V2 Replacement Tank']
-    },
-    {
-        name: 'Accessories/Miscellaneous',
-        synonyms: ['wire','drip tip','cotton','apparel','mod accessories','pens','wick','adapter',
-        'screwdriver','tweezer','decorative ring','magnet connector','vaper twizer','diy tool kit','Clapton Coil Building Kit','Zipper Storage Bag','Mouthpiece Glass']
-    }
-]
+        try{         
+            const raw_products = execute_scrape ? await scrape(domain, _data_dir, raw_products_file, utils, log) : utils.readJSON(_data_dir, raw_products_file, log)
+            const cleaned_products = clean(raw_products, domain, buckets, utils, log)
+            execute_inventory && utils.writeJSON(utils.INVENTORIES_DIR, inventory_file, cleaned_products, log)
+        }catch(err){
+            log.error(err)
+        }finally{
+            const time_finish = Date.now()
+            log.info("processed " +domain+ " execution in " + (time_finish - time_start)/1000 + " seconds")
+            resolve() 
+        }
+    })
+}
 
-function clean(raw_products){
+function clean(raw_products, domain, buckets, utils, log){
 
     let includes = [
         'E-Liquid',
@@ -72,6 +49,8 @@ function clean(raw_products){
         'Chargers', 
         'Batteries',]
 
+        log.info(`//////////cleaning raw products: count: ${raw_products.length}////////////////`)
+
         raw_products = raw_products
         .map( (p)=>{
             p.product_type = p.product_type.split(" - ").join(",")
@@ -83,7 +62,7 @@ function clean(raw_products){
             return {
                 id:             p.id,  
                 name:           p.title,
-                src:            `${DOMAIN}/products/${p.handle}`,    
+                src:            `${domain}/products/${p.handle}`,    
                 brand:          p.vendor,
                 category:       p.product_type,
                 img:            p.images && p.images.length > 0 ? p.images[0].src : null,
@@ -93,96 +72,35 @@ function clean(raw_products){
         .map( 
             (p)=>{              // add product to 0 or more buckets, based on tags/synomyns within each bucket. print no bucket or multibucket matches to log
                 p.buckets = utils.getProductBuckets(p.category, p.name, buckets)
-                p.buckets.length > 1 && logger.info(`multiple buckets: ${p.buckets} , ${p.name}, ${p.category}`)
-                p.buckets.length === 0 && logger.info(`multiple buckets: ${p.buckets} , ${p.name}, ${p.category}`)
+                p.buckets.length > 1 && log.info(`multiple buckets: ${p.buckets} , ${p.name}, ${p.category}`)
+                p.buckets.length === 0 && log.info(`no buckets: ${p.buckets} , ${p.name}, ${p.category}`)
                 //console.log(p.buckets , p.name, p.category)
                 return p})
 
-    //TODO: the 'tag' array has lots of descriptive tags that would simplify categorizing/branding products from shopify-based stores
+    log.info(`//////////finished cleaning: count: ${raw_products.length} ////////////////`)
+
     return raw_products
 }
 
-module.exports = ( () => {
-    return new Promise( async (resolve) => {
-        logger.info("**************************executing " +DOMAIN+ " process***********************************************")
+async function scrape(domain, dir, file_name, utils, logger){
 
-        const time_start = Date.now()
+    const start_page = 1
+    const end_page = 6
 
-        try{
-            const start_page = 1
-            const end_page = 6
+    const subpath = 'products.json'
+    const page_param = page => `page=${page}`
+    const limit_param = 'limit=250'
 
-            const subpath = 'products.json'
-            const page_param = (page)=>`page=${page}`
-            const limit_param = 'limit=250'
+    const urls = []
 
-            const urls = []
+    //generate urls
+    for(let page = start_page; page <= end_page; page++)
+        urls.push(`${domain}/${subpath}?${limit_param}&${page_param(page)}`)
 
-            //generate urls
-            for(let page = start_page; page <= end_page; page++)
-                urls.push(`${DOMAIN}/${subpath}?${limit_param}&${page_param(page)}`)
-        
-            //visit urls, process each url with scraper function, return array of array of products
-           // const products = (await utils.scrapePages(urls, json=>json["products"],logger)).flat()
-           
-            //utils.writeJSON(DATA_DIR, RAW_PRODUCTS_FILE_NAME, products, logger)
-            const products = utils.readJSON(DATA_DIR, RAW_PRODUCTS_FILE_NAME, logger)
-
-           clean(products)
-
-
-            utils.writeJSON(utils.INVENTORIES_DIR, INVENTORY_FILE_NAME, clean(products) , logger)
-
-        }catch(err){
-            logger.error(err)
-        }finally{
-            const time_finish = Date.now()
-            logger.info("processed " +DOMAIN+ " execution in " + (time_finish - time_start)/1000 + " seconds")
-            resolve() 
-        }
-    })
-})()
-
-
-/*
-async function execute(){
-
-    return new Promise( (resolve) => {
-
-        logger.info("**************************executing " +DOMAIN+ " process***********************************************")
-
-        const time_start = Date.now()
-
-        const start_page = 1
-        const end_page = 6
-
-        const subpath = 'products.json'
-        const page_param = (page)=>`page=${page}`
-        const limit_param = 'limit=250'
-
-        const urls = []
-
-        //generate urls
-        for(let page = start_page; page <= end_page; page++)
-            urls.push(`${DOMAIN}/${subpath}?${limit_param}&${page_param(page)}`)
-        
-        //visit urls, process each url with scraper function, return array of products
-        utils.scrapePages(urls, json=>json["products"],logger)
-        .then( 
-            (products) => {
-                products = products.flat()
-                utils.writeJSON(DATA_DIR, ALL_PRODUCTS_FILE_NAME, products, logger)
-                utils.writeJSON(utils.INVENTORIES_DIR, INVENTORY_FILE_NAME, products.map( product=>parseProduct(product)), logger)
-        })
-        .catch( (err) => logger.error(err))
-        .finally( ()=>{
-            const time_finish = Date.now()
-            logger.info("processed " +DOMAIN+ " execution in " + (time_finish - time_start)/1000 + " seconds")
-            resolve() 
-        })
-    })
+    //visit urls, process each url with scraper function, return array of array of products
+    const products = (await utils.scrapePages(urls, json=>json["products"],logger)).flat()
+    utils.writeJSON(dir, file_name, products, logger)
+    return products
 }
 
-module.exports = { execute }
 
-*/
