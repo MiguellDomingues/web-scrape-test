94e8798e6c24669dbbb3cd425c84c4067549480e
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-13 18:37:38 -0800

    clean up

--- scraper/scripts/inventory.js
+++ scraper/scripts/inventory.js
@@ -5,52 +5,7 @@ const createProducts = require('../../database/create/createProducts.js')
 
 const LOG_FILE_NAME  = 'inventory'
 
-const logger = utils.getLogger(LOG_FILE_NAME)
-
-const buckets = [
-    {
-        name: 'Juices',
-        synonyms: ['e-juice', //surreyvapes
-                   'ejuice',  //ezvape
-                   'e-liquid'] //tbvapes
-    },
-    {
-        name: 'Coils',
-        synonyms: ['coil','rda','atomizer']
-    },
-    {
-        name: 'Pods',
-        synonyms: ['pod',]
-    },
-    {
-        name: 'Tanks',
-        synonyms: ['tank','clearomizer']
-    },
-    {
-        name: 'Starter Kits',
-        synonyms: ['starter', 'kit','disposable']
-    },
-    {
-        name: 'Mods',
-        synonyms: ['boxes', 'boxmod', 'box mod', 'mod', 'box']
-    },
-    {
-        name: 'Batteries',
-        synonyms: ['battery', 'batteries',]
-    },
-    {
-        name: 'Chargers',
-        synonyms: ['charger','charging']
-    },
-    {
-        name: 'Replacement Glass',
-        synonyms: ['glass','replacement','pyrex','replacement glass']
-    },
-    {
-        name: 'Accessories/Miscellaneous',
-        synonyms: ['wire','drip tip','cotton','apparel','mod accessories','pens','wick','adapter','screwdriver','tweezer','decorative ring','magnet connector','vaper twizer']
-    },      
-]
+const log = utils.getLogger(LOG_FILE_NAME)
 
 let vape_flavours = [
     'Arctic Jungle',
@@ -95,123 +50,44 @@ function normalize(product, categories, source){
         }  
 }
 
-
 module.exports = ( () => {
 
-    logger.info("***************** reading JSON files in " + utils.INVENTORIES_DIR+" ********************")
+    log.info("***************** reading JSON files in " + utils.INVENTORIES_DIR+" ********************")
 
     const getFileName = file => file.split('.')[0]
     const inventories_dir = fs.readdirSync(utils.INVENTORIES_DIR);
 
     if(inventories_dir.length < 1){
-        logger.error(utils.INVENTORIES_DIR+ " has no files. aborting")
+        log.error(utils.INVENTORIES_DIR+ " has no files. aborting")
         return
     }
 
-    logger.info("************ files: "+ inventories_dir.map(f => `${getFileName(f)}.json`).join(',') + "********************")
+    log.info("************ files: "+ inventories_dir.map(f => `${getFileName(f)}.json`).join(',') + "********************")
 
     const normalized_products = []
 
-    
-
-    const merged_maps = utils.getMap(logger)
-    let total_products = 0
+    const pbm = utils.initProductBucketMetrics(log)
 
     inventories_dir.forEach( (file)=>{
-        //normalized_products.push(normalizeProducts(utils.readJSON(utils.INVENTORIES_DIR, getFileName(file), logger), getFileName(file)))
-        //normalized_products.push(utils.readJSON(utils.INVENTORIES_DIR, getFileName(file), logger))
+        
+        const products = utils.readJSON(utils.INVENTORIES_DIR, getFileName(file), log)
+        log.info("/////////////////////////////"+ getFileName(file)+ "///////////////////////////////////////")
 
-        const products = utils.readJSON(utils.INVENTORIES_DIR, getFileName(file), logger)
-        total_products+=products.length
-       
-
-        //return the matching bucket names, if any, for a product
-        function getProductBuckets(category_str, name_str){
-
-            /*
-            calculate the bucket points for each bucket by scanning the product category/name strs for keywords
-            return an object of the form { "bucketname1":{points: int}, "bucketname2":{points: int}, ... }
-            */
-            const bucket_points = ( (category_str, name_str) =>{
-
-                //count the occurances of src string inside the trgt string string
-                const countMatches = (trgt, src) => {
-                    const result = trgt.match(new RegExp(src, 'g'))
-                    return result ? result.length : 0
-                }
-
-                const bucket_points = {}
-
-                buckets.forEach( (bucket)=>{
-                    bucket_points[bucket.name] = { points: 0}
-                    bucket.synonyms.forEach( (synonym)=>{
-                        let points = bucket_points[bucket.name].points
-                        points = points + countMatches(category_str, synonym) + countMatches(name_str, synonym)
-                        bucket_points[bucket.name].points = points
-                    })
-                })
-
-                return bucket_points
-            })(category_str, name_str)
-
-            /*
-            return 0-n max score bucket name(s)
-            if multiple buckets were tied for max score, return those bucket names
-            if all the scores were 0, returns an empty list     
-            */
-            const max_points_buckets = ( (bp)=>{
-                const non_zero_points = Object.keys(bp).filter( k => bp[k].points > 0)                                                
-                const sorted_by_max_points = non_zero_points.sort( (lhs, rhs)=> bp[rhs].points - bp[lhs].points)
-                const max_points_bucket = sorted_by_max_points.shift()
-                return max_points_bucket ? [max_points_bucket, ...sorted_by_max_points.filter( k => bp[k].points === bp[max_points_bucket].points )] : []
-            })(bucket_points)
-
-            return max_points_buckets
-        }
-
-        logger.info("/////////////////////////////"+ getFileName(file)+ "///////////////////////////////////////")
-
-        const map = utils.getMap(logger)
-        let tie_matches_count = 0
-
-        products.forEach( (p)=>{
-
-            let matches = getProductBuckets(p.category.toLowerCase(), p.name.toLowerCase())
-
-            if(matches.length === 0){        
-                logger.info(p.category+ " "+ p.name+ "has no bucket")
-            }else if(matches.length > 1){
-                logger.info("multiple matches: ("+ matches + ") " + p.category+ " | "+ p.name) //if there was a tie among buckets, add the same product to multiple buckets
-                map.put(matches[0],p)
-                merged_maps.put(matches[0],p)
-                tie_matches_count++
-            }else{
-                map.put(matches[0],p)
-                merged_maps.put(matches[0],p)
-            } 
-
-            normalized_products.push(normalize(p, [matches[0]], getFileName(file)))
-        })
-        logger.info("/////////////////////////////BUCKETS///////////////////////////////////////")
-        map.print()
-       
+        products.forEach( (p) => {
+            pbm.putProductBuckets([p.buckets[0]], p)
+            normalized_products.push(normalize(p, [p.buckets[0]], getFileName(file)))
+        })  
     })
 
-    logger.info("/////////////////////////////BUCKETS FOR ALL PRODUCTS///////////////////////////////////////")
+    pbm.printProductBuckets()
 
-    merged_maps.print()
-
-    //console.log(normalized_products)
-    //console.log(normalized_products.length)
-    
     createProducts(normalized_products).then((db_result)=>{
-        logger.info("INSERTED " + db_result.length + " RECORDS")
+        log.info("INSERTED " + db_result.length + " RECORDS")
     }).catch((err)=>{
-        logger.error("err from database")
-        logger.error(err)
+        log.error("err from database")
+        log.error(err)
     })  
     
-
 })()
 
 /*
