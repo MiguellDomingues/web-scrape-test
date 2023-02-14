db97fcbb2f75e0642153de0e409046700a91a2b4
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-14 14:14:26 -0800

    added query to insert documents into tagMetaData

--- database/create/createProducts.js
+++ database/create/createProducts.js
@@ -1,7 +1,8 @@
 const { Product } = require('../models.js');
+const { TagMetaData } = require('../models.js');
 let db = require('../database.js')
 
-module.exports = async function createProducts(products) {
+async function createProducts(products) {
     return new Promise( (resolve, reject) => {
          db.connect().then( ()=>{
             Product.insertMany(products)
@@ -12,4 +13,18 @@ module.exports = async function createProducts(products) {
         }).catch( (err)=> { reject(new Error("Database connection Error", { cause: err }) ) });
     })
  }
+
+ async function createTagMetaData(tmd) {
+    return new Promise( (resolve, reject) => {
+         db.connect().then( ()=>{
+            TagMetaData.insertMany(tmd)
+            .then( (result) => { resolve(result)} )
+            .catch( (err) =>  { reject(new Error("Query Error", { cause: err })) } )
+            .finally( ()=> { db.disconnect()} )
+    
+        }).catch( (err)=> { reject(new Error("Database connection Error", { cause: err }) ) });
+    })
+ }
+
+ module.exports = { createProducts, createTagMetaData }
 
