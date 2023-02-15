a3756b8c915b83271ea2d11067b0dfd76736b9e4
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-15 11:45:42 -0800

    added filtering with category/product/brand tags, added DropDownMenu cmp

--- vape-finder/src/components/searchBar/SearchBar.js
+++ vape-finder/src/components/searchBar/SearchBar.js
@@ -1,28 +1,111 @@
 import './search_bar.css'
 
-import {useState} from 'react'
-
-const categories = ['Juices','Coils','Pods','Tanks','Starter Kits','Mods','Batteries','Chargers','Replacement Glass','Accessories/Miscellaneous',]
-    
-const _brands = [
-    'Allo', 'GORE', 'JUUL', 'Voopoo'
-]
-
-const _stores = [
-    'EZVAPE','SURREYVAPES', 'THUNDERBIRDVAPES'
-]
+import { useSearchBar } from './useSearchBar.js'
 
 function SearchBar( {refetch} ){
+
+    const [
+       filter_tags,
+       selected_filters,
+        loading,
+        error,
+        {
+            selectedFilterBGC,
+            onCategorySelected,
+            onStoreSelected,
+            onBrandSelected 
+        }
+     ] = useSearchBar(refetch)
+
+    const { category_tags, brands_tags, stores_tags } = filter_tags
+    const { category, stores, brands, } = selected_filters
+
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+
+    return (<>
+
+        <div className="search_bar_categories">
+            {category_tags.map( (c, idx) =>
+            <div>
+                <span className={"search_bar_category cursor_hand" + selectedFilterBGC(category, [c.tag_name])} 
+                      key={idx} 
+                      onClick={ () => onCategorySelected(c.tag_name)}>
+                    {c.tag_name}              
+                </span>
+                <span className="search_bar_product_count">{c.product_count}</span>
+            </div>)}
+        </div>
+
+        <div className="search_bar_shops_about">
+
+        <DropDownMenu title="Brands" tags={brands_tags} selected_tags={brands} selectedHandler={onBrandSelected} selectedTagsBGC={selectedFilterBGC}/>
+        <DropDownMenu title="Stores" tags={stores_tags} selected_tags={stores} selectedHandler={onStoreSelected} selectedTagsBGC={selectedFilterBGC}/>
+                {/*
+        <div className="shops cursor_hand">
+                Shops
+                <div className="dropdown-arrow"></div>
+                <div className="dropdown-content">    
+                    {stores_tags.map( (store, idx)=>
+                    <span className={selectedFilterBGC(store.tag_name, stores)}  
+                          key={idx} 
+                          onClick={ () => onStoreSelected(store.tag_name) }>
+                         {store.tag_name} {store.product_count}
+                    </span>)}                  
+                </div>
+        </div>   
+        
+                    */  }
+           
+           <span className="about cursor_hand">About</span> 
+        </div>
+    </>)
+}
+
+export default SearchBar
+
+function DropDownMenu( {title, tags, selected_tags, selectedHandler, selectedTagsBGC} ){
+
+    return(<div className="brands cursor_hand">
+    {title}
+    <div className="dropdown-arrow"></div>
+    <div className="dropdown-content">    
+        {tags.map( (tag, idx)=>
+            <span className={selectedTagsBGC(tag.tag_name, selected_tags)}
+                  key={idx} 
+                  onClick={ ()=> selectedHandler(tag.tag_name)}>
+                 {tag.tag_name} {tag.product_count}          
+            </span>)}                  
+    </div>
+</div>)
+}
+
+
+ /*
 
    const [category, setCategory] = useState("");
    const [stores, setStores] = useState([]);
    const [brands, setBrands] = useState([]);
 
-   const echo = () => console.log(category, stores, brands)
+   const query = useQuery(GET_SEARCH_TYPES);
 
-   echo()
+   const { data, loading, error } = query
 
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+
+   console.log("////", data, loading, error)
+
+   const orderTagsByProductCount = (tags) => tags.sort( (lhs, rhs)=> rhs.product_count-lhs.product_count)
+   const filterTagsByMinProductCount = (tags, min_product_count = 0) => tags.filter((tag) => tag.product_count >= min_product_count)
    const selectedFilterBGC = (str, arr) => arr.includes(str) ? " filter_selected" : "" 
+
+   const category_tags = orderTagsByProductCount( filterTagsByMinProductCount (data["getSearchTypes"][0].tags))
+   const brands_tags = orderTagsByProductCount( filterTagsByMinProductCount (data["getSearchTypes"][1].tags, 5))
+   const stores_tags =  orderTagsByProductCount( filterTagsByMinProductCount (data["getSearchTypes"][2].tags))
+
+   const echo = () => console.log(category, stores, brands)
+   echo()
 
    const onCategorySelected = (_category) => {
      const __category = _category === category ? "" : _category
@@ -42,47 +125,4 @@ function SearchBar( {refetch} ){
      
      refetch(category, _brands, stores)
    }
-
-return (<>
-
-        <div className="search_bar_categories">
-            {categories.map( (_category, idx) =>
-                <span className={"search_bar_category cursor_hand" + selectedFilterBGC(category, [_category])} 
-                      key={idx} 
-                      onClick={ () => onCategorySelected(_category) }>
-                    {_category}  
-                </span>)}
-        </div>
-
-        <div className="search_bar_shops_about">
-            <div className="brands cursor_hand">
-                Brands
-                <div className="dropdown-arrow"></div>
-                <div className="dropdown-content">    
-                    {_brands.map( (brand, idx)=>
-                        <span className={selectedFilterBGC(brand, brands)}
-                              key={idx} 
-                              onClick={ ()=> onBrandSelected(brand) }>
-                             {brand}
-                        </span>)}                  
-                </div>
-            </div>
-            <div className="shops cursor_hand">
-                Shops
-                <div className="dropdown-arrow"></div>
-                <div className="dropdown-content">    
-                    {_stores.map( (shop, idx)=>
-                    <span className={selectedFilterBGC(shop, stores)}  
-                          key={idx} 
-                          onClick={ () => onStoreSelected(shop) }>
-                         {shop}
-                    </span>)}                  
-                </div>
-            </div>        
-           
-           <span className="about cursor_hand">About</span> 
-        </div>
-    </>)
-}
-
-export default SearchBar
+   */
