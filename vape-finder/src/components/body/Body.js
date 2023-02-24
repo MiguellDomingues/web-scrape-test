84730909375ca9926835a5b53ad06d2c4c956e77
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-23 18:54:17 -0800

    added loading screen, mock select input, to ui

--- vape-finder/src/components/body/Body.js
+++ vape-finder/src/components/body/Body.js
@@ -1,18 +1,28 @@
 import './body.css'
 import CardList from '.././cardList/CardList.js'
+import LoadingOverlay from 'react-loading-overlay'
 
 function Body( {query} ) {
 
-  const { loading, error, data } = query
+  let { loading, error, data } = query
 
-  if (loading) return 'Loading...';
+  console.log("data: ", data)
+  console.log("loading: ", loading)
+
+ // if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
+
+  //data ? data["getProducts"] : []
 
   const { fetchMore } = query
 
-  return (<main className="body">
-    <CardList products={data["getProducts"]} fetchMore={fetchMore}/>
-  </main>);
+  return(
+  <LoadingOverlay active={loading} spinner text=''>
+    <main className="body">
+      <CardList products={data ? data["getProducts"] : []} fetchMore={fetchMore} loading={loading}/>
+    </main>
+  </LoadingOverlay>
+  );
 }
 
 export default Body;
--- vape-finder/src/components/cardList/CardList.js
+++ vape-finder/src/components/cardList/CardList.js
@@ -2,7 +2,7 @@ import './cardlist.css'
 import Card from '../card/Card'
 import {useState} from 'react'
 
-function CardList( { products, fetchMore } ) {
+function CardList( { products, fetchMore, loading } ) {
 
  
   const [last_product_id, setLPID] = useState("");
@@ -24,10 +24,17 @@ function CardList( { products, fetchMore } ) {
     }
   }
 
-  console.log(products)
+ // console.log(products)
 
   return (
     <div className="card_container" id="cardContainer" onScroll={ e => isBottom(e) && hasProducts(products) && handleBottom() }>  
+        {!loading && 
+          <select className='dropdown'>
+            <option value="none">None</option>
+            <option value="high_to_low">Price: High to Low</option>
+            <option value="low_to_high">Price: Low to High</option>
+          </select>
+        }
         {products.map( (product, idx)=> 
           <Card key={idx} product={product}/>)}     
     </div>
