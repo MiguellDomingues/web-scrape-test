5961161cd6f5ad895ab728d3e0b1106a017a7462
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-02-24 17:13:55 -0800

    added health warning image to header

--- vape-finder/src/components/header/Header.js
+++ vape-finder/src/components/header/Header.js
@@ -1,12 +1,20 @@
 import './header.css'
 import SearchBar from '../searchBar/SearchBar.js'
 
+const img_src = '../../../health_warning.webp';
+
 function Header( {refetch} ) {
   //console.log(refetch)
     return ( 
     <><div className="container_header">
 
       <div className="top">
+
+        <img className="warning_img"
+          src={img_src}
+          alt="Health_Warning">
+        </img>
+     
         <span className="header_title">BC VAPE FINDER</span>
       </div>
 
